819c07a44b7cbb7c9d7f1326c01bf395ccf133cb
Austin Lim <alaustinall@gmail.com>
2026-01-30 20:09:24 -0800

    Set up

---
+++ app/(pages)/layout.jsx
@@ -0,0 +1,20 @@
+import "./_globals/globals.scss";
+import { Oxanium } from 'next/font/google'
+
+export const metadata = {
+  title: "Space and Satellite Systems",
+  description: "Space and Satellite Systems club at UC Davis",
+  icons: "./favicon.png"
+};
+
+const oxanium = Oxanium();
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={oxanium.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
