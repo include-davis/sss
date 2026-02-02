6f18915b2e477cbb6b5d362be457088fda75249c
austin1im <alaustinall@gmail.com>
2026-02-01 18:38:18 -0800

    Example

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,9 +1,11 @@
 import styles from "./page.module.scss"
+import Test from "../_components/test";
 
 export default function Home() {
   return (
     <main>
-      <h1 className={styles.test}>Home</h1>
+      <div className={styles.test}>Home</div>
+      <Test title="title" text="text"/>
     </main>
   );
 }
