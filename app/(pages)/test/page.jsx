53275934c1a6f5d4bbbd8b556c7fe20d8fd2b831
StevenFireRose777 <stevennuno769@gmail.com>
2026-02-06 20:10:02 -0800

    inital

---
+++ app/(pages)/test/page.jsx
@@ -0,0 +1,22 @@
+"use cilent"; 
+import { usestate } from  "react"
+import Image from "next/image"
+import styles from "./page.module.scss"
+// import styles from "./mixins.scss"
+
+// import .scss file (use styles)
+
+
+
+export default function software()
+{
+
+  // js code outside of return
+
+  return(
+    // put html stuff inside div
+  <div>
+        <p>test</p>
+  </div>
+  );
+}
