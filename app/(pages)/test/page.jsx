a0e794ddbbf8838bedefad2e3ce4bdcf53295f6f
StevenFireRose777 <stevennuno769@gmail.com>
2026-02-12 15:31:00 -0800

    final #1

--- app/(pages)/test/page.jsx
+++ app/(pages)/test/page.jsx
@@ -1,7 +1,8 @@
-"use cilent";
-import { usestate } from "react";
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Link from "next/link";
 // import styles from "./mixins.scss"
 
 // import .scss file (use styles)
@@ -12,13 +13,54 @@ export default function software() {
   return (
     // put html stuff inside div
     <div className={styles.pageBody}>
-
       <div className={styles.content}>
-        <h1 className={styles.softwareHeader}>Software Work</h1>
-        <p className={styles.softwareDescription}>Our Software team focuses mainly on the embedded software and writing our custom operating system on the orbital platform called IntelliSat. IntelliSat is written fully in C, has bare metal drivers, and uses FreeRTOS to schedule mission tasks. Work in Software involves using STM32CubeIDE, looking at datasheets, documentation, looking at schematics, and embedded C</p>
-        <button>IntelliSat - GitHub</button>
-        <button>Radio Controller - GitHub</button>
-        <button>MGT Controller - GitHub</button>
+        <div className={styles.LeftContainer}>
+          <h1 className={styles.softwareHeader}>Software Work</h1>
+          <p className={styles.softwareDescription}>
+            Our Software team focuses mainly on the embedded software and
+            writing our custom operating system on the orbital platform called
+            IntelliSat. IntelliSat is written fully in C, has bare metal
+            drivers, and uses FreeRTOS to schedule mission tasks. Work in
+            Software involves using STM32CubeIDE, looking at datasheets,
+            documentation, looking at schematics, and embedded C
+          </p>
+
+          <div className={styles.SSSButtonsContainer}>
+            <Link
+              href={
+                "https://github.com/Space-and-Satellite-Systems-UC-Davis/IntelliSat"
+              }
+              className={styles.SSSButton}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              IntelliSat - GitHub
+            </Link>
+
+            <Link
+              href={
+                "https://github.com/Space-and-Satellite-Systems-UC-Davis/REALOP_Radio_Controller"
+              }
+              className={styles.SSSButton}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Radio Controller - GitHub
+            </Link>
+            <Link
+              href={
+                "https://github.com/Space-and-Satellite-Systems-UC-Davis/REALOP_MGT_Controller"
+              }
+              className={styles.SSSButton}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              MGT Controller - GitHub
+            </Link>
+
+          </div>
+        </div>
+
         <div className={styles.SVGContainer}>
           <svg
             width="429"
@@ -34,7 +76,6 @@ export default function software() {
           </svg>
         </div>
       </div>
-
     </div>
   );
 }
