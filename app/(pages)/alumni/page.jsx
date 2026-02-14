88fc26aaf92219a478f30e398213708c1c79ff64
Lily Lam <lilylammm2006@gmail.com>
2026-02-14 15:37:11 -0800

    alumni

---
+++ app/(pages)/alumni/page.jsx
@@ -0,0 +1,34 @@
+
+import styles from "./alumni.module.scss"
+// className={styles.main_title}
+
+export default function Alumni() {
+  return (
+    <main>
+      <div className={styles.main_title}>
+        <p>Where our Alumni have gone</p>
+      </div>
+      <div>
+        <div>
+          1
+        </div>
+        <div>
+          2
+        </div>
+        <div>
+          3
+        </div>
+        <div>
+          4
+        </div>
+        <div>
+          5
+        </div>
+        <div>
+          6
+        </div>
+      </div>
+
+    </main>
+  );
+}
