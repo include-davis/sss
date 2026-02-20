3973241c750b5919505ad4c517f72cf5a0a85103
Miguel Beltran <migueljoaquinbeltran@gmail.com>
2026-02-19 17:04:17 -0800

    Fixed styling, Added test page

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,17 +1,8 @@
 import styles from "./page.module.scss"
-import Mission from "../_components/mission/mission";
 
 export default function Home() {
   return (
     <main>
-      <Mission
-        title="Mission"
-        subtitle="Investigate a low cost alternative to traditional reaction wheels."
-        paragraphs={[
-          "The goal is to break down some of the financial barriers that keep teams from experimentation via CubeSats. In following our core belief to prioritize the education of our members, almost all components are designed from scratch, avoiding COTS parts when possible.",
-          "Realop 1 is SSS's first cubesat with the mission of testing computer hard disk drives as a low cost alternative to reaction wheels."
-        ]}
-      />
     </main>
   );
 }
