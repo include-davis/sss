fd3f4dbd47db04fc1ab3fa2739e9f06608d23a99
Miguel Beltran <migueljoaquinbeltran@gmail.com>
2026-02-19 23:13:17 -0800

    Fixed styling, added test page

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,14 +1,8 @@
 import styles from "./page.module.scss"
-import WhyJoinSSS from "../_components/why-join-sss/why-join-sss"
 
 export default function Home() {
   return (
     <main>
-      <WhyJoinSSS
-        heading="Why join SSS?"
-        paragraph1="Join a community and network who truly enjoy engineering and making things!"
-        paragraph2="We make it a priority to make sure all members have the opportunity to have meaningful contributions to the project and learn industry relevant skills. We always have plenty to do!"
-      />
     </main>
   );
 }
---
+++ app/(pages)/test/page.jsx
@@ -0,0 +1,13 @@
+import WhyJoinSSS from "../_components/why-join-sss/why-join-sss"
+
+export default function TestPage() {
+  return (
+    <main>
+      <WhyJoinSSS
+        heading="Why join SSS?"
+        paragraph1="Join a community and network who truly enjoy engineering and making things!"
+        paragraph2="We make it a priority to make sure all members have the opportunity to have meaningful contributions to the project and learn industry relevant skills. We always have plenty to do!"
+      />
+    </main>
+  );
+}
