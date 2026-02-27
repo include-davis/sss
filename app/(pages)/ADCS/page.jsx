f1d44953d36ad141a8ea323cb4952602242026e4
StevenFireRose777 <stevennuno769@gmail.com>
2026-02-27 11:47:11 -0800

    draft #2

--- app/(pages)/ADCS/page.jsx
+++ app/(pages)/ADCS/page.jsx
@@ -1,25 +1,52 @@
-import styles from "./ADCS.module.scss"
+import styles from "./ADCS.module.scss";
+import Image from "next/image";
 
-
-
-export default function ADCS()
-{
-  return(
-
+export default function ADCS() {
+  return (
     <div>
-
       <div className={styles.TopContent}>
         <h1>ADCS Work</h1>
-        <p>ADCS is the mission of the satellite itself and represents our interdisciplinary team that does work in electrical, structures, and software.</p>
-        <p>They have been hard at working doing HDD testing, creating our coils, using sun sensors to figure out satellite orientation, and writing algorithms to handle the data and outputs of the various components on the satellite.</p>
+        <p>
+          ADCS is the mission of the satellite itself and represents our
+          interdisciplinary team that does work in electrical, structures, and
+          software.
+        </p>
+        <p>
+          They have been hard at working doing HDD testing, creating our coils,
+          using sun sensors to figure out satellite orientation, and writing
+          algorithms to handle the data and outputs of the various components on
+          the satellite.
+        </p>
       </div>
 
-      <div className={styles.BottomContent}>
-        
+      <div className={styles.ImageCarosuel}>
+        <div className={styles.Group}>
+          <div className={styles.card}>
+            <Image
+              src={"/public/sunsensor.png"}
+              width={500}
+              height={500}
+              alt="Picture of Sun Sensor Testing Rig"
+            />
+          </div>
+          <div className={styles.card}>
+            <Image
+              src={"/public/sunsensor.png"}
+              width={500}
+              height={500}
+              alt="Picture of Sun Sensor Testing Rig"
+            />
+          </div>
+          <div className={styles.card}>
+            <Image
+              src={"/public/sunsensor.png"}
+              width={500}
+              height={500}
+              alt="Picture of Sun Sensor Testing Rig"
+            />
+          </div>
+        </div>
       </div>
-
     </div>
-
   );
-  
 }
