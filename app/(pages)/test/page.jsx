edb00662e778478f4eb91a4bdc679257e2095f95
jordanxwu <jordanwu1430@gmail.com>
2026-03-05 17:26:10 -0800

    [Structure Work]
    - Developed by Jordan WU

--- app/(pages)/test/page.jsx
+++ app/(pages)/test/page.jsx
@@ -1,8 +1,10 @@
+import StructureWork from '../_components/structureWork/structurework';
 import PastMissions from '../_components/pastMissions/pastMissions';
 
 export default function Home() {
   return (
     <main>
+      <StructureWork />
       <PastMissions />
     </main>
   );
