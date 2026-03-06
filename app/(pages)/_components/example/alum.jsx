7121f545be2c26b633cdbfb81ac7b370147902d4
Lily Lam <lilylammm2006@gmail.com>
2026-03-05 18:01:31 -0800

    alumni

--- app/(pages)/_components/example/alum.jsx
+++ app/(pages)/_components/example/alum.jsx
@@ -6,34 +6,36 @@ import Image from "next/image";
 export default function Alumni(){
 
   const images = [
-    {src: "/lockheed.svg", height: 120, width: 360, alt: "Lockheed Martin"},
-    {src: "/boing.svg", height: 70, width: 360, alt: "Boing"},
-    {src: "/JPL.svg", height: 100, width: 360, alt: "Jet Propulsion Laboratory"},
-    {src: "/apple.svg", height: 120, width: 360, alt: "Apple"},
-    {src: "/nasa.svg", height: 110, width: 360, alt: "NASA"},
-    {src: "/divergent.svg", height: 100, width: 360, alt: "Divergent"},
+    {src: "/lockheed.svg", height: 108, width: 360, alt: "Lockheed Martin"},
+    {src: "/boing.svg", height: 65, width: 379, alt: "Boing"},
+    {src: "/JPL.svg", height: 106, width: 377, alt: "Jet Propulsion Laboratory"},
+    {src: "/apple.svg", height: 171, width: 304, alt: "Apple"},
+    {src: "/nasa.svg", height: 156, width: 183, alt: "NASA"},
+    {src: "/divergent.svg", height: 40, width: 318, alt: "Divergent"},
   ]
 
 
   const images2 = [
-    {src: "/northrop.svg", height: 130, width: 360, alt: "Northrop Grumman"},
-    {src: "/maxar.svg", height: 110, width: 360, alt: "Maxar"},
-    {src: "/nasa2.svg", height: 130, width: 360, alt: "NASA2"},
-    {src: "/amd.svg", height: 130, width: 360, alt: "AMD"},
-    {src: "/lawrence.svg", height: 130, width: 360, alt: "Lawrence Livermore National Laboratory"},
-    {src: "/rtx.svg", height: 90, width: 360, alt: "RTX"},
-    {src: "/cheveron.svg", height: 150, width: 360, alt: "Cheveron"},
+    {src: "/northrop.svg", height: 193, width: 343, alt: "Northrop Grumman"},
+    {src: "/maxar.svg", height: 128, width: 303, alt: "Maxar"},
+    {src: "/nasa2.svg", height: 187, width: 194, alt: "NASA2"},
+    {src: "/amd.svg", height: 167, width: 268, alt: "AMD"},
+    {src: "/lawrence.svg", height: 66, width: 339, alt: "Lawrence Livermore National Laboratory"},
+    {src: "/rtx.svg", height: 108, width: 280, alt: "RTX"},
+    {src: "/cheveron.svg", height: 206, width: 275, alt: "Cheveron"},
   ]
 
   return (
-    <main>
+    <main className={styles.main}>
       <h1 className={styles.header}>Where our Alumni have gone</h1>
+
       <div className={styles.carousel}>
+
         <div className={styles.group}>
           {images.map((item, index) => (
             <div key={index} className={styles.box}>
               <div className={styles.image}>
-                <Image src={item.src} height={item.height} width={item.width} alt={item.alt}/>
+                <Image className={styles.logo} src={item.src} height={item.height} width={item.width} alt={item.alt}/>
               </div>
             </div>
           ))}  
@@ -43,11 +45,12 @@ export default function Alumni(){
           {images.map((item, index) => (
             <div key={index} className={styles.box}>
               <div className={styles.image}>
-                <Image src={item.src} height={item.height} width={item.width} alt={item.alt}/>
+                <Image className={styles.logo} src={item.src} height={item.height} width={item.width} alt={item.alt}/>
               </div>
             </div>
           ))}  
         </div>
+        
       </div>
 
 
@@ -56,7 +59,7 @@ export default function Alumni(){
         <div className={styles.group2}>
           {images2.map((item, index) => (
             <div key={index} className={styles.box}>
-                <Image src={item.src} height={item.height} width={item.width} alt={item.alt}/>
+                <Image className={styles.logo} src={item.src} height={item.height} width={item.width} alt={item.alt}/>
             </div>
           ))}  
         </div>
@@ -64,7 +67,7 @@ export default function Alumni(){
         <div aria-hidden className={styles.group2}>
           {images2.map((item, index) => (
             <div key={index} className={styles.box}>
-                <Image src={item.src} height={item.height} width={item.width} alt={item.alt}/>
+                <Image className={styles.logo} src={item.src} height={item.height} width={item.width} alt={item.alt}/>
             </div>
           ))}  
         </div>
