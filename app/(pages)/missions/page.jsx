8bfd2c939656b741db46b873f59f281c6ba1a992
austin1im <alaustinall@gmail.com>
2026-03-05 17:54:10 -0800

    merge adjustments

---
+++ app/(pages)/missions/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.scss"
+import PastMissions from "../_components/pastMissions/pastMissions";
+
+export default function Missions() {
+  return (
+    <main>
+      <h1 className={styles.test}>Missions</h1>
+      <PastMissions/>
+    </main>
+  );
+}
--- app/(pages)/test/page.jsx
+++ app/(pages)/test/page.jsx
@@ -1,6 +1,8 @@
-import HowCanYouJoin from '../_components/howcanyoujoin/howCanYouJoin';
+import HowCanYouJoin from '../_components/howCanYouJoin/howCanYouJoin';
 import StructureWork from '../_components/structureWork/structurework';
 import PastMissions from '../_components/pastMissions/pastMissions';
+
+// leave this page and all conflicts on here shouldn't really matter, we'll delete this when all prs are merged
 
 export default function Home() {
   return (
