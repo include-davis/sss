09e21c57265c9d107a54183e4567426c5453abb1
Kayla Yu Chen <kayla.chen03@gmail.com>
2026-03-08 19:58:38 -0700

    [Navbar]
    - Developed by Kayla Chen

--- app/(pages)/test/page.jsx
+++ app/(pages)/test/page.jsx
@@ -1,3 +1,21 @@
+import Navbar from "../../_components/Navbar/Nav.jsx";
+export default function Page() {
+    return (
+      <main>
+        <Navbar
+        logoSrc="/favicon.png"
+        logoAlt="Space and Satellite Systems"
+        pages={[
+          { label: "Home", href: "/" },
+          { label: "Missions", href: "/missions" },
+          { label: "Teams", href: "/teams" },
+          { label: "Sponsor Us", href: "/sponsor" },
+          { label: "Join Us", href: "/join" },
+        ]}
+        />
+      </main>
+    );
+  }
 import HowCanYouJoin from "../_components/HowCanYouJoin/howCanYouJoin";
 import StructureWork from "../_components/StructureWork/structurework";
 import PastMissions from "../_components/PastMissions/pastMissions";
