3226f7eb3fd5c0cd48490280cf0dfdb6e4897546
Yaohong Chen <139181004+Yc103@users.noreply.github.com>
2026-03-10 16:41:52 -0700

    [Why You Should Sponsor]
    - Developed by Yaohong Chen

---
+++ app/(pages)/why_sponsor_us/page.jsx
@@ -0,0 +1,25 @@
+import styles from './page.module.scss';
+import Image from 'next/image';
+
+export default function Whysponsorus(){
+    return(
+        <div className={styles.container}>
+            <div className={styles.textPictureContainer}>
+            {/*use this if path to image don't work <Image src="/public/Mask-group 1.png" alt="Team Picture" className={styles.picture} ></Image>*/}
+                <img src="/Mask-group_1.png" alt="TeamPicture" className={styles.picture} ></img>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.SponsorUsTitle}>Why should you sponsor us?</h1>
+                        <ul className={styles.benefit}>
+                            <li>Tax Benefits for a charitable donation</li>
+                            <li>Every dollar is put into the club to train members</li>
+                            <li>Invest in the next generation of engineers</li>
+                            <li>Help Students connect theory and pracitcal</li>
+                            <li>Recruit from our club for interns or full time offers</li>
+                        </ul>
+                </div>
+            </div>
+        </div>
+
+
+    );
+}  
