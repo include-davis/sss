3b26c179f14378f016731e4519e745c2111f0b4e
Ryan Truong <ryan.truong00@gmail.com>
2026-03-09 17:07:02 -0700

    [Electrical Work]
    - Developed by Ryan Truong

--- app/(pages)/test/page.jsx
+++ app/(pages)/test/page.jsx
@@ -1,6 +1,7 @@
 import HowCanYouJoin from "../_components/HowCanYouJoin/howCanYouJoin";
 import StructureWork from "../_components/StructureWork/structurework";
 import PastMissions from "../_components/PastMissions/pastMissions";
+import ElectricalWork from '../_components/electricalWork/electricalWork';
 import WhatWereUpTo from "../_components/WhatWereUpTo/whatWereUpTo";
 
 // leave this page and all conflicts on here shouldn't really matter, we'll delete this when all prs are merged
@@ -11,6 +12,7 @@ export default function Home() {
       <HowCanYouJoin />
       <StructureWork />
       <PastMissions />
+      <ElectricalWork />
       <WhatWereUpTo />
     </main>
   );
