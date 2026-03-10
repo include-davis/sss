530ef43b538802923f1b2a43fc465381d0d96362
Naomi Zhao <naomitan.zhao@gmail.com>
2026-03-09 17:33:01 -0700

    Adjust hero styling and implement on Mission spage

--- app/(pages)/missions/page.jsx
+++ app/(pages)/missions/page.jsx
@@ -1,11 +1,21 @@
 import styles from "./page.module.scss";
 import PastMissions from "../_components/PastMissions/pastMissions";
+import Hero from "../_components/Hero/Hero";
+import Image from "next/image";
 
 export default function Missions() {
   return (
     <main>
-      <h1 className={styles.test}>Missions</h1>
+      <Hero textContent={<HeroText />} imageContent={<HeroImage />} />
       <PastMissions />
     </main>
   );
+}
+
+function HeroText() {
+  return <h1 className={styles.heroText}>Missions</h1>;
+}
+
+function HeroImage() {
+  return <Image src="/missions_hero_graphic.svg" fill />;
 }
