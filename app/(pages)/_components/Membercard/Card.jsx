4674111ad11a5bce8b0e0facac31ef3c98b96cac
Kayla Yu Chen <kayla.chen03@gmail.com>
2026-03-10 17:50:43 -0700

    [Member Cards]
    - Developed by Kayla Yu Chen

---
+++ app/(pages)/_components/Membercard/Card.jsx
@@ -0,0 +1,38 @@
+import styles from "./Card.module.scss";
+import { Mail } from "lucide-react";
+import Image from "next/image";
+
+export default function Membercard({
+    name,
+    title,
+    department,
+    image,
+    emailLink
+}) {
+    return (
+        <div className={styles.card}>
+                    <Image
+                    className={styles.image}
+                    src={image || "/images/avatar-placeholder.svg"} 
+                    alt={name}
+                    width={244}
+                    height={251}/>
+            
+
+            <div className={styles.info}>
+                <div className={styles.topRow}>
+                    <h3 className={styles.name}>{name}</h3>
+
+                    {emailLink && (
+                        <a href={`mailto:${emailLink}`} className={styles.email}>
+                            <Mail size ={27} />
+                        </a>
+                    )}
+                </div>
+
+                <p className={styles.title}>{title}</p>
+                <p className={styles.department}>{department}</p>
+            </div>
+        </div>
+    );
+}
